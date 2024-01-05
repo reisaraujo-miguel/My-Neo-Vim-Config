6538fddaf274fa99d10ef968c961ce8d7639cc50
reisaraujo-miguel <reisaraujo.miguel@gmail.com>
2024-01-05 18:24:29 -0300

    some updates

--- lua/leug/lazy.lua
+++ lua/leug/lazy.lua
@@ -1,100 +1,100 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		'git',
+		'clone',
+		'--filter=blob:none',
+		'https://github.com/folke/lazy.nvim.git',
+		'--branch=stable', -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 
-require("lazy").setup({
-    'tpope/vim-fugitive',
+require('lazy').setup({
+	{ 'tpope/vim-fugitive' },
 
-    'norcalli/nvim-colorizer.lua',
+	{ 'norcalli/nvim-colorizer.lua' },
 
-    'rust-lang/rust.vim',
+	{ 'rust-lang/rust.vim' },
 
-    'simrat39/rust-tools.nvim',
+	{ 'simrat39/rust-tools.nvim' },
 
-    {
-        'nvim-telescope/telescope.nvim', tag = '0.1.3',
-        -- or                              , branch = '0.1.x',
-        dependencies = { 'nvim-lua/plenary.nvim' }
-    },
+	{
+		'nvim-telescope/telescope.nvim',
+		tag = '0.1.3',
+		-- or                              , branch = '0.1.x',
+		dependencies = { 'nvim-lua/plenary.nvim' }
+	},
 
-    {
-        'akinsho/flutter-tools.nvim',
-        lazy = false,
-        dependencies = {
-            'nvim-lua/plenary.nvim',
-            'stevearc/dressing.nvim', -- optional for vim.ui.select
-        },
-        config = true,
-    },
+	{
+		'akinsho/flutter-tools.nvim',
+		lazy = false,
+		dependencies = {
+			'nvim-lua/plenary.nvim',
+			'stevearc/dressing.nvim', -- optional for vim.ui.select
+		},
+		config = true,
+	},
 
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{ { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 } },
 
-    {
-        'akinsho/bufferline.nvim',
-        version = "*",
-        dependencies = 'nvim-tree/nvim-web-devicons',
-        after = 'catppuccin',
-        config = function()
-            require('bufferline').setup {
-                highlights = require("catppuccin.groups.integrations.bufferline").get()
-            }
-        end
-    },
+	{
+		'akinsho/bufferline.nvim',
+		version = '*',
+		dependencies = { 'nvim-tree/nvim-web-devicons', },
+		config = function()
+			require('bufferline').setup {
+				highlights = require('catppuccin.groups.integrations.bufferline').get(),
+			}
+		end,
+	},
 
-    {
-        'nvim-lualine/lualine.nvim',
-        dependencies = { 'nvim-tree/nvim-web-devicons', opt = true },
-    },
+	{
+		'nvim-lualine/lualine.nvim',
+		dependencies = { 'nvim-tree/nvim-web-devicons', },
+	},
 
-    {{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}},
+	{ { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' }, },
 
 
-    {
-        'windwp/nvim-autopairs',
-        event = "InsertEnter",
-        opts = {} -- this is equalent to setup({}) function
-    },
+	{
+		'windwp/nvim-autopairs',
+		event = 'InsertEnter',
+		opts = {}, -- this is equalent to setup({}) function
+	},
 
-    {
-        "windwp/nvim-ts-autotag",
-        wants = "nvim-treesitter",
-        config = function()
-            require("nvim-ts-autotag").setup { enable = true }
-        end,
-    },
+	{
+		'windwp/nvim-ts-autotag',
+		dependencies = { { 'nvim-treesitter' }, },
+		opts = function(_, opts)
+			opts.autotag = { enable = true, }
+		end,
+	},
 
-    {
-        {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+	{
+		{ { 'VonHeikemen/lsp-zero.nvim', branch = 'v3.x' } },
 
-        {'williamboman/mason.nvim'},
-        {'williamboman/mason-lspconfig.nvim'},
+		{ 'williamboman/mason.nvim' },
+		{ 'williamboman/mason-lspconfig.nvim' },
 
-        -- LSP Support
-        {
-            'neovim/nvim-lspconfig',
-            dependencies = {
-                {'hrsh7th/cmp-nvim-lsp'},
-            },
-        },
+		-- LSP Support
+		{
+			'neovim/nvim-lspconfig',
+			dependencies = {
+				{ 'hrsh7th/cmp-nvim-lsp' },
+			},
+		},
 
-        -- Autocompletion
-        {
-            'hrsh7th/nvim-cmp',
-            dependencies = {
-                {'L3MON4D3/LuaSnip'},
-            }
-        }
-    }
+		-- Autocompletion
+		{
+			'hrsh7th/nvim-cmp',
+			dependencies = {
+				{ 'L3MON4D3/LuaSnip' },
+			}
+		}
+	}
 })
 
-vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
+vim.g.mapleader = ' ' -- Make sure to set `mapleader` before lazy so your mappings are correct
--- lua/leug/set.lua
+++ lua/leug/set.lua
@@ -3,8 +3,7 @@ vim.opt.nu = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-
+vim.opt.expandtab = false
 vim.opt.smartindent = true
 
 vim.opt.hlsearch = false
